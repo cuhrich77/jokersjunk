ff7bf6fb01ff38a2a5c76e433882b0f20a74d500
cuhrich77 <cuhrich77@gmail.com>
2026-04-25 10:59:13 -0400

    Update places.js

--- api/places.js
+++ api/places.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   if (!input || input.length < 3) {
     return res.status(400).json({ predictions: [] });
   }
-
   const apiKey = process.env.GOOGLE_MAPS_KEY;
 
   try {
