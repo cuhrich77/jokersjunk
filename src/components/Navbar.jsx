1594aa270ec020e2417d12370c3ba63f86f8aad4
cuhrich77 <cuhrich77@gmail.com>
2026-05-06 18:06:23 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar() {
 
       <Link to="/" style={{display:'flex',alignItems:'center',gap:12}}>
   <img
-    src="/jokersjunklogo.jpg"
+    src="/Jokersjunklogo1.png"
     alt="Jokers Junk Removal"
     style={{height:'80px', width:'auto'}}
   />
