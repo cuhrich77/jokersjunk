d461380cc9c41730d5fe2f7aed8fb4c9963b3566
cuhrich77 <cuhrich77@gmail.com>
2026-07-04 01:42:20 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', padding: '24px 16px 0' }}>
   <img
-    src="/jokersjunklogo.jpg"
+    src="/Jokersjunklogo.png"
     alt="Jokers Junk Removal"
     style={{ height: '160px', width: 'auto' }}
   />
